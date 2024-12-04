aea65fc6ba42ab70a34783d7f68e7fa443a744fe
Tridiktya Putra <tridiktya@gmail.com>
2024-12-04 11:16:32 +0700

    fix: swagger api docs server

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const fs = require('fs');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 
@@ -15,29 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors())
 
-app.get('/swagger.json', (req, res) => {
-    const { protocol, headers } = req;
-
-    // read swagger.json file
-    const swaggerTemplate = JSON.parse(fs.readFileSync('./src/docs/swagger.json', 'utf8'));
-
-    // add dynamic server
-    swaggerTemplate.servers = [
-    {
-        url: `${protocol}://${req.get('host')}`,
-        description: "Current server"
-    }
-    ];
-
-    res.json(swaggerTemplate);
-});
-
 // serve swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, { 
-    swaggerOptions: {
-        url: '/swagger.json' // Swagger JSON endpoint
-    }
-}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
