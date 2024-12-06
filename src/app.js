5d86af45729b728a7fa4110699323a9893d4197e
Usman Hidayatulloh Sidiq <usmanhdsidiq@gmail.com>
2024-12-06 13:45:20 +0700

    Fix redundant after rebase

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const passwordRoutes = require("./routes/password.routes");
 const userRoutes = require("./routes/user.routes");
 const oauthRoutes = require("./routes/oauth.routes");
 require("./services/removeJwt");
+const favDestinationRoutes = require("./routes/fav.destination.routes");
 
 dotenv.config();
 const prisma = new PrismaClient();
@@ -17,7 +18,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 app.use(cors());
 app.use(express.json());
 
@@ -25,20 +25,13 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
 const router = require("./routes/router");
-=======
-const favDestinationRoutes = require('./routes/favDestination.routes');
->>>>>>> e398786 (API Destinasi Favorit)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-<<<<<<< HEAD
 // serve swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-=======
-app.use('/api/home', favDestinationRoutes);
->>>>>>> e398786 (API Destinasi Favorit)
 
 app.use(
   session({
@@ -55,6 +48,8 @@ app.use("/api/v1", authRoutes);
 app.use("/api/v1", passwordRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/", oauthRoutes);
+
+app.use("/api/home", favDestinationRoutes);
 
 app.get("/", (req, res) => {
   res.send("E-Flight Ticket Platform Backend");
