57de35be251981f4145990606a804aa9f733bc80
Usman Hidayatulloh Sidiq <usmanhdsidiq@gmail.com>
2024-12-06 13:09:31 +0700

    Move to folder favorite

--- src/routes/favDestination.routes.js
+++ src/routes/favDestination.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const favDestinationController = require('../controllers/favDestination.controller');
+const favDestinationController = require('../controllers/favorite/favDestination.controller');
 const router = express.Router();
 
 router.get('/favorite-destination', (req, res) => favDestinationController.favDestination(req, res));
