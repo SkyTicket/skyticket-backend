6fb3a8759b67804ccec0c00855becb3924d45e35
ahazain <ahmadfariddd5@gmail.com>
2024-12-07 11:51:27 +0700

    add auth in seatControllers

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth/auth.controller");
-// const { authMiddleware, authorizeRoles } = require("../middleware/auth");
+
 
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,13 +7,23 @@ const FlightsController = require("../controllers/flights/controllers/flights.co
 const SeatController = require("../controllers/orderTiket/seatControllers");
 const TiketController = require("../controllers/orderTiket/ticketControllers");
 const PaymentController = require("../controllers/orderTiket/transaksiControllers");
+const {authMiddleware} = require("../middleware/auth");
 
+//search & list tiket
 app.get("/", IndexController.index);
 app.get("/api/v1/flights", FlightsController.searchFlights);
 app.get("/api/v1/airports", AirportsController.searchAirports);
-app.get("/api/v1/seat", SeatController.getDetailFlight);
+
+//order-tiket
+app.get(
+  "/api/v1/seat",
+  authMiddleware,
+  SeatController.getDetailFlight
+);
 app.post("/ticket-order", TiketController.createTicketOrder);
 app.post("/create-payment", PaymentController.createPayment);
-app.post("/midtrans-notification", PaymentController.paymentNotification);
+app.post(
+  "/midtrans-notification",PaymentController.paymentNotification
+);
 
 module.exports = app;
