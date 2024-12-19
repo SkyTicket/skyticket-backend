1ee5c8f04eefed379cbe681604c66f42c131f2e0
Usman Hidayatulloh Sidiq <usmanhdsidiq@gmail.com>
2024-12-20 01:17:44 +0700

    Create favorite.test.js

---
+++ src/tests/favorite.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const prisma = require('./mocks/prisma');
+const { expect } = require('@jest/globals');
+
+const pageNumber = parseInt(page);
+const pageLimit = parseInt(limit);
+const skip = (pageNumber - 1) * pageLimit;
+
+description('GET /api/v1/favorite-destination', () => {
+    test('Menampilkan destinasi favorit', async () => {
+        const response = await request(app).get('/api/v1/favorite-destination?page=1&continent=asia');
+
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Data tidak ditemukan', async () => {
+        const response = await request(app).get('/api/v1/favorite-destination?page=1&continent=java');
+
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('Kesalahan pada server', async () => {
+        prisma.destinations.findMany.mockRejectedValue(new Error('Database error'));
+
+        const response = await request(app).get('/api/v1/favorite-destination?page=1&continent=asia');
+
+        expect(response.statusCode).toBe(500);
+    });
+})
